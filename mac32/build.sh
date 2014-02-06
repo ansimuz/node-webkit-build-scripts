3c959dda2a3bd0c17d150c9023c8fac3349f10b8
jviotti <juanchiviotti@gmail.com>
2014-02-06 10:14:14 -0400

    Copy mac icon

--- mac32/build.sh
+++ mac32/build.sh
@@ -12,12 +12,13 @@ if [[ -z "$NW_FILE"  ]]
 then
   echo "Missing nw package"
   exit 1
-fi  
+fi
 
 [ ! -d $DIST_DIR  ] && mkdir -p $DIST_DIR
 
 cp -rf $NODE_WEBKIT $CACHE_DIR
 unzip -o $NW_FILE -d $CACHE_DIR/Contents/Resources/app.nw
+cp nw.icns $CACHE_DIR/Contents/Resources/nw.icns
 rm -rf $DIST_DIR/$APP_NAME.app
 mv $CACHE_DIR $DIST_DIR/$APP_NAME.app
 
